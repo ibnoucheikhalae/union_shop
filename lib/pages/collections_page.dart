ecf9c046c93c71c12ca929e234e1904a1ae5f846
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-11-28 10:33:41 +0100

    Add shared footer to CollectionsPage

--- lib/pages/collections_page.dart
+++ lib/pages/collections_page.dart
@@ -53,6 +53,28 @@ class CollectionsPage extends StatelessWidget {
 
   ],
 ),
+ Container(
+              width: double.infinity,
+              color: Colors.grey[50],
+              padding: const EdgeInsets.all(24),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Placeholder Footer',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'footer section',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
 
               ),
             ),
