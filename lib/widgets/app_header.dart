14c1d3373442596070f160ed88f3260bdd226573
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-11-28 08:49:49 +0100

    Add initial AppHeader widget skeleton

---
+++ lib/widgets/app_header.dart
@@ -0,0 +1,11 @@
+// lib/widgets/app_header.dart
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // placeholder
+  }
+}
