ffd7e3d13bc4df01e4566703b3167aff9e1e6329
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-11-28 09:36:00 +0100

    Integrate AppHeader into AboutPage

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import '../widgets/app_header.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
+  void _placeholder() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("AboutPage placeholder")),
+    return Scaffold(
+      body: Column(
+        children: [
+          AppHeader(
+            onLogoTap: () {},
+            onSearchTap: _placeholder,
+            onAccountTap: _placeholder,
+            onCartTap: _placeholder,
+            onMenuTap: _placeholder,
+          ),
+        ],
+      ),
     );
   }
 }
