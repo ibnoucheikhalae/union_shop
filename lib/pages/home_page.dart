4a61e0db5868fd3a954462d02dec053e9a3212d1
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 15:44:53 +0000

    Create HomePage and widget with navigation methods

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+// lib/pages/home_page.dart
+import 'package:flutter/material.dart';
+import '../widgets/footer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  void navigateToHome(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  void navigateToProduct(BuildContext context) {
+    Navigator.pushNamed(context, '/product');
+  }
+
+  void navigateToCollections(BuildContext context) {
+    Navigator.pushNamed(context, '/collections');
+  }
+
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
+
+  
