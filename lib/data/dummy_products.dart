1efbbe90ffc4e9d8c7d961e875a4c270d547810c
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:07:35 +0000

    Add dummy product data for Classic Uni Hoodie

---
+++ lib/data/dummy_products.dart
@@ -0,0 +1,11 @@
+import '../models/product.dart';
+
+
+
+const dummyProducts = [
+  Product(
+    id: 'hoodie-1',
+    title: 'Classic Uni Hoodie',
+    price: '£35.00',
+    image: 'assets/products/classic_hoodie.png',
+  ),
