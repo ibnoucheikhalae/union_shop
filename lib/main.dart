9bf5d590f914cc5f72ce38e94b1769ff82c396a6
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:29:20 +0000

    Add placeholder header to HomeScreen for future updates

--- lib/main.dart
+++ lib/main.dart
@@ -66,4 +66,14 @@ class HomeScreen extends StatelessWidget {
               height: 100,
               color: Colors.white,
               child: Column(
-                children: [
+                children: [// Top banner
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    color: const Color(0xFF4d2963),
+                    child: const Text(
+                      'PLACEHOLDER HEADER TEXT - STUDENTS TO UPDATE!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
