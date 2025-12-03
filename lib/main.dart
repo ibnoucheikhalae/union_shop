b16dd8272ef1cffd7d048647e1a9db2b5910ab51
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:28:21 +0000

    Add HomeScreen widget/class with navigation methods for improved routing

--- lib/main.dart
+++ lib/main.dart
@@ -37,3 +37,21 @@ class UnionShopApp extends StatelessWidget {
     );
   }
 }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  void navigateToHome(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  void navigateToProduct(BuildContext context) {
+    Navigator.pushNamed(context, '/product');
+  }
+
+  void navigateToCollections(BuildContext context) {
+    Navigator.pushNamed(context, '/collections');
+  }
+
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
