f7cfe0f53d0b4866e9706bbe6219f0ae5463c19f
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 23:22:28 +0000

    Add CartItem model for shopping cart

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,19 +1,24 @@
 // lib/models/cart_item.dart
-import '../models/product.dart';
+import 'product.dart';
 
 class CartItem {
   final Product product;
+  final String colour;
+  final String size;
   int quantity;
 
   CartItem({
     required this.product,
+    required this.colour,
+    required this.size,
     this.quantity = 1,
   });
 
-  double get unitPrice {
+  // Parse "£6.99" -> 6.99
+  double get unitPriceValue {
     final cleaned = product.price.replaceAll(RegExp(r'[^0-9.]'), '');
     return double.tryParse(cleaned) ?? 0.0;
   }
 
-  double get totalPrice => unitPrice * quantity;
+  double get lineTotal => unitPriceValue * quantity;
 }
