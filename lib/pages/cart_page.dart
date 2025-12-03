375ea2628c5b3aa2cc3e209391aa135bad45c838
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 17:03:45 +0000

    Add CartPage widget with basic structure and navigation

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,3 +2,22 @@
 import 'package:flutter/material.dart';
 import '../widgets/app_header.dart';
 import '../widgets/footer.dart';
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  void _placeholder() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppHeader(
+              onLogoTap: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/',
+                  (route) => false,
+                );
+              },
