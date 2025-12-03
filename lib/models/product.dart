687c54d298519071b3b70bc57bd63a29a21b8a37
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 19:03:16 +0000

    Add collectionSlug to ProductPage

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,12 +3,12 @@ class Product {
   final String id;
   final String title;
   final String price;
-  final String image;
+  final String collectionSlug; // links to a collection
 
   const Product({
     required this.id,
     required this.title,
     required this.price,
-    required this.image,
+    required this.collectionSlug,
   });
 }
