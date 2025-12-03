b532b27481333b6a52d06b614dfeb068e76e732c
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 12:40:20 +0000

    Add AppHeader to CollectionViewPage

--- lib/pages/collection_view_page.dart
+++ lib/pages/collection_view_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import '../widgets/app_header.dart';
 
 class CollectionViewPage extends StatelessWidget {
   const CollectionViewPage({super.key});
 
+  void _placeholder() {}
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Collection View')),
+    return Scaffold(
+      body: Column(
+        children: [
+          AppHeader(
+            onLogoTap: () {
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/',
+                (route) => false,
+              );
+            },
+            onSearchTap: _placeholder,
+            onAccountTap: _placeholder,
+            onCartTap: _placeholder,
+            onMenuTap: _placeholder,
+          ),
+          const Expanded(
+            child: Center(child: Text("Collection content goes here")),
+          ),
+        ],
+      ),
     );
   }
 }
