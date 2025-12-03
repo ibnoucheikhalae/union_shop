815e67a9e404641b71e69c03f96aba0413e95daf
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:21:21 +0000

    Create Proudct Card File and Add ProductCard widget for displaying product details

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,14 @@
+// lib/widgets/product_card.dart
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imageUrl;
+
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
