4f72b37a818c26a270f90e2a94df0cc12fcb8ce1
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:07:43 +0000

    Create Product class for product data representation

---
+++ lib/models/product.dart
@@ -0,0 +1,14 @@
+// lib/models/product.dart
+class Product {
+  final String id;
+  final String title;
+  final String price;
+  final String image;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
+  });
+}
