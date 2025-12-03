6ec8c12c43e323b3ffc633d73d94f1a58af60ccf
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 15:30:59 +0000

    Implement AppFooter widget with basic layout

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -2,3 +2,15 @@ import 'package:flutter/material.dart';
 
 class AppFooter extends StatelessWidget {
   const AppFooter({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.grey[50],
+      padding: const EdgeInsets.all(24),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Placeholder Footer',
+            style: TextStyle(
