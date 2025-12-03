e8d6fbc74083ca58d68f8353fca5c072fc8abce9
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 18:36:43 +0000

    Add dummy collections data

---
+++ lib/data/dummy_collections.dart
@@ -0,0 +1,11 @@
+// lib/data/dummy_collections.dart
+import '../models/collection.dart';
+
+const List<Collection> dummyCollections = [
+  Collection(
+    id: 'c1',
+    slug: 'hoodies',
+    title: 'Hoodies & Sweatshirts',
+    description: 'Warm and comfy uni hoodies for everyday campus life.',
+    category: 'clothing',
+  ),
