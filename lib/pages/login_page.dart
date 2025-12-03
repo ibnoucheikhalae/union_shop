52351f3b60ea3600e120a143473d3654421485e6
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 14:15:14 +0000

    Add placeholder method and update build structure in LoginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,3 +3,19 @@ import '../widgets/app_header.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
+
+void _placeholder() {}
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppHeader(
+              onLogoTap: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/',
+                  (route) => false,
+                );
+              },
