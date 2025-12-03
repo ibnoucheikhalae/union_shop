c7687b6dd0b478f75417970b780381822c781389
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:33:38 +0000

    Refactor main.dart to intial state

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 // lib/main.dart
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:union_shop/pages/product_page.dart';
-import 'package:union_shop/pages/about_page.dart';
-import 'package:union_shop/pages/collections_page.dart';
-import 'package:union_shop/pages/collection_view_page.dart';
-import 'package:union_shop/pages/sale_page.dart';
-import 'package:union_shop/widgets/product_card.dart';
+
+import 'pages/home_page.dart';
+import 'pages/product_page.dart';
+import 'pages/about_page.dart';
+import 'pages/collections_page.dart';
+import 'pages/collection_view_page.dart';
+import 'pages/sale_page.dart';
+import 'pages/signup_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -21,9 +23,7 @@ class UnionShopApp extends StatelessWidget {
       title: 'Union Shop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF4d2963),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4d2963)),
       ),
       initialRoute: '/',
       routes: {
@@ -37,73 +37,3 @@ class UnionShopApp extends StatelessWidget {
     );
   }
 }
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  void navigateToHome(BuildContext context) {
-    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-  }
-
-  void navigateToProduct(BuildContext context) {
-    Navigator.pushNamed(context, '/product');
-  }
-
-  void navigateToCollections(BuildContext context) {
-    Navigator.pushNamed(context, '/collections');
-  }
-
-  void placeholderCallbackForButtons() {
-    // This is the event handler for buttons that don't work yet
-  }
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Header
-            Container(
-              height: 100,
-              color: Colors.white,
-              child: Column(
-                children: [// Top banner
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    color: const Color(0xFF4d2963),
-                    child: const Text(
-                      'PLACEHOLDER HEADER TEXT - STUDENTS TO UPDATE!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-
-                                  // Main header
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              navigateToHome(context);
-                            },
- child: Image.network(
-                              'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
-                              height: 18,
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Container(
-                                  color: Colors.grey[300],
-                                  width: 18,
-                                  height: 18,
-                                  child: const Center(
-                                    child: Icon(
-                                      Icons.image_not_supported,
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
