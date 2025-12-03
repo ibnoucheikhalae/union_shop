0710444d5fb7a9716ba10268b40865b87a304909
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-03 16:30:22 +0000

    Add image widget to HomeScreen with error handling

--- lib/main.dart
+++ lib/main.dart
@@ -88,3 +88,22 @@ class HomeScreen extends StatelessWidget {
                             onTap: () {
                               navigateToHome(context);
                             },
+ child: Image.network(
+                              'https://shop.upsu.net/cdn/shop/files/upsu_300x300.png?v=1614735854',
+                              height: 18,
+                              fit: BoxFit.cover,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Container(
+                                  color: Colors.grey[300],
+                                  width: 18,
+                                  height: 18,
+                                  child: const Center(
+                                    child: Icon(
+                                      Icons.image_not_supported,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
