438b2c191db5098af8876094ce15442dee74cf55
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-04 15:38:49 +0000

    Implement build method in PrintShackAboutPage for rendering UI

--- lib/pages/printshack_about_page.dart
+++ lib/pages/printshack_about_page.dart
@@ -6,3 +6,22 @@ class PrintShackAboutPage extends StatelessWidget {
   const PrintShackAboutPage({super.key});
 
   void _placeholder() {}
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppHeader(
+              onLogoTap: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/',
+                  (route) => false,
+                );
+              },
+              onSearchTap: _placeholder,
+              onAccountTap: () => Navigator.pushNamed(context, '/login'),
+              onCartTap: () => Navigator.pushNamed(context, '/cart'),
+              onMenuTap: _placeholder,
+            ),
