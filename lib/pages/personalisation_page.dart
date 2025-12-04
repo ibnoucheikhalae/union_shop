b8d7ec7218a30c86d30a2ee63f046041747a1a55
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-04 15:48:51 +0000

    Create PersonalisationPage with initial structure and imports

---
+++ lib/pages/personalisation_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../widgets/app_header.dart';
+import '../widgets/footer.dart';
+import '../services/cart_service.dart';
+import '../models/product.dart';
+
+class PersonalisationPage extends StatefulWidget {
+  const PersonalisationPage({super.key});
+
+  @override
+  State<PersonalisationPage> createState() => _PersonalisationPageState();
+}
