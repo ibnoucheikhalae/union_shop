5e4c4feccb5c3cf7fa7bd713382cb021ea702778
alae ibnou cheikh <alaeibnoucheikh@alaes-MacBook-Air.local>
2025-12-04 15:38:29 +0000

    Create a printshack about page

---
+++ lib/pages/printshack_about_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../widgets/app_header.dart';
+import '../widgets/footer.dart';
+
+class PrintShackAboutPage extends StatelessWidget {
+  const PrintShackAboutPage({super.key});
+
+  void _placeholder() {}
